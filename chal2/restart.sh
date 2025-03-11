8db6665d03afe1265f269fd8a41e0cd9f2620a4e
Lukas Peters <peterslukas0512@gmail.com>
2025-03-11 17:25:56 +0000

    chal2 stuff

--- chal2/restart.sh
+++ chal2/restart.sh
@@ -2,4 +2,4 @@
 sudo docker stop graphdata2
 sudo docker remove graphdata2
 sudo docker build -t graphdata2 .
-sudo docker run --name graphdata1 -d -p 4001:4001 graphdata2
+sudo docker run --name graphdata2 -d -p 4001:4001 graphdata2
